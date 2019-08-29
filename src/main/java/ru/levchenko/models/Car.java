7a8fd00c7288b009b4dfac6f75ad7d754e9d614e
NikolayLev <niko.original2@gmail.com>
2019-08-29 20:35:17 +0500

    Adding Car Class

---
+++ src/main/java/ru/levchenko/models/Car.java
@@ -0,0 +1,32 @@
+package ru.levchenko.models;
+
+public class Car {
+    private User owner;
+    private String model;
+    private Integer maxSpeed;
+
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(Integer maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
